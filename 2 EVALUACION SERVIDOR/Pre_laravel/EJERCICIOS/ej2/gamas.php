4cfe4973fddb4d048943e096509227b121e8fb4c
victorArcosMiro <145359912+victorArcosMiro@users.noreply.github.com>
2024-01-30 15:58:45 +0100

    a

--- 2 EVALUACION SERVIDOR/Pre_laravel/EJERCICIOS/ej2/gamas.php
+++ 2 EVALUACION SERVIDOR/Pre_laravel/EJERCICIOS/ej2/gamas.php
@@ -5,7 +5,7 @@ include_once "model.php";
 class Gamas extends Model{
     public function __construct()
     {
-        parent::__construct("gamas");
+        parent::__construct("gamasproductos");
     }
 }
 ?>
